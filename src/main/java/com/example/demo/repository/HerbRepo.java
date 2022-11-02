2a3d6661c260da4f626be7bfa453127fcde2ee80
katmcn <ekaterina.mcneill@gmail.com>
2022-11-02 15:52:56 +0000

    update. query added

--- src/main/java/com/example/demo/repository/HerbRepo.java
+++ src/main/java/com/example/demo/repository/HerbRepo.java
@@ -1,12 +1,25 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Herb;
 
+import java.util.Optional;
+
+
 @Repository
 public interface HerbRepo extends JpaRepository<Herb, Long>{
 
+	Herb findHerbByName(String name); 
+	 
+	 @Query(value = "SELECT * FROM HERB WHERE name=?1 LIMIT 1", nativeQuery = true)
+		Optional<Herb> getByName(String name);
+	 
+	
+
 }
+
+
 
